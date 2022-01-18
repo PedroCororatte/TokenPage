235de787ad3ae70d8dd04ac189206f3e8378ec05
PedroCororatte <pedroagt112@gmail.com>
2022-01-18 19:28:24 -0300

    Base project

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+export default function page(){
+    return(
+        <div>
+            <h1>Welcome next level cryptocurrency token with faster transfer</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolore magna ipsum dolor sit amet consectetur.</p>
+        </div>
+    )
+} 
