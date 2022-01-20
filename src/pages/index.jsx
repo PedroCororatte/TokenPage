347df3a99f1c9ff66f28f0ee6289ffb8b7b84b9a
PedroCororatte <pedroagt112@gmail.com>
2022-01-20 00:36:55 -0300

    navBar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,28 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-export default function page(){
-    return(
-        <div>
-            <h1>Welcome next level cryptocurrency token with faster transfer</h1>
+export default function page() {
+  return (
+    <>
+      <div className="Header">
+        <div className="Title">
+          <h1>CryptoToken</h1>
         </div>
-    )
-} 
+        <div className="Nav">
+          <h4>Home</h4>
+          <h4>KeyFeatures</h4>
+          <h4>Found Rising</h4>
+          <h4>FAQ</h4>
+          <button className="btnTypeA">Try for free</button>
+        </div>
+      </div>
+      <div className="Main">
+        <h1>Welcome next level cryptocurrency token with faster transfer</h1>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore
+        </h4>
+      </div>
+    </>
+  );
+}
