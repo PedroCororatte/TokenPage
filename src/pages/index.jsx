7f11230d00ef26cdc0e899a3b20c2103fe1e501f
PedroCororatte <pedroagt112@gmail.com>
2022-01-20 00:32:23 -0300

    navbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,6 @@ export default function page(){
     return(
         <div>
             <h1>Welcome next level cryptocurrency token with faster transfer</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolore magna ipsum dolor sit amet consectetur.</p>
         </div>
     )
 } 
